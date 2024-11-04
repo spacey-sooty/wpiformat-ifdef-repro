b4959615e6478cd2cf8e6d28603c7d212b354598
Jade Turner <spacey-sooty@proton.me>
2024-11-04 11:12:14 +0800

    clang format uneeded
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- main.cpp
+++ main.cpp
@@ -2,9 +2,7 @@
 
 #ifdef _WIN32
 #include <memory>
-void func() {
-  std::unique_ptr<char[]> buffer = std::make_unique<char[]>("");
-}
+void func() { std::unique_ptr<char[]> buffer = std::make_unique<char[]>(""); }
 #else
 void func() {}
 #endif
